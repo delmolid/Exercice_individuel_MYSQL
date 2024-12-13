5cf737aae90b13cdfcdbb03b10ac1eb68130ec2f
Molid <delmolid@hotmail.fr>
2024-12-13 15:53:45 +0100

    suppression de la table edusign et creation d'une nouvelle table edusign ajout des signatures dans le bon ordre

--- edusign.sql
+++ edusign.sql
@@ -6,6 +6,7 @@
 -- email TEXT NOT NULL UNIQUE 
 -- );
 
+-- Création de la table edusign
 -- CREATE TABLE edusign (
 -- id INTEGER PRIMARY KEY AUTOINCREMENT,
 -- firstname TEXT NOT NULL,
@@ -13,15 +14,72 @@
 -- created_at DATETIME NOT NULL
 -- );
 
+-- Création des apprenantes dans la table users
 -- INSERT INTO users (firstname, lastname, email) VALUES ('Ada', 'Lovelace', 'ada@test.fr');
 -- INSERT INTO users (firstname, lastname, email) VALUES ('Paul', 'Martin', 'paul@test.fr');
 -- INSERT INTO users (firstname, lastname, email) VALUES ('Celine', 'Pierre', 'celine@test.fr');
 -- INSERT INTO users (firstname, lastname, email) VALUES ('Awaleh', 'Samatar', 'awaleh@test.fr');
 
--- SELECT * FROM users;
--- SELECT * FROM edusign;
+-- Suppression de la prémiere table edusign 
 --DROP TABLE edusign;
- -- SELECT * FROM edusign;
+
+-- Création d'une nouvelle table edusign avec un FOREIGN KEY (user_id)
+--  CREATE TABLE edusign (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_id INTEGER NOT NULL,
+--     created_at DATETIME NOT NULL,
+--     FOREIGN KEY (user_id) REFERENCES users(id)
+-- );
+
+ -- Mise à jour des apprenantes de la table user afin de suivre l'énonce de l'excercice
+ -- UPDATE users SET firstname='Beatrice' WHERE id=2;
+-- UPDATE users SET lastname='Worsley' WHERE id=2;
+-- UPDATE users SET email='bea@test.fr' WHERE id=2;
+-- UPDATE users SET firstname='Bella' WHERE id=3;
+-- UPDATE users SET lastname='Guerin' WHERE id=3;
+-- UPDATE users SET email='bella@test.fr' WHERE id=3;
+-- UPDATE users SET firstname='Barbara' WHERE id=4;
+-- UPDATE users SET lastname='Chase' WHERE id=4;
+-- UPDATE users SET email='barbara@test.fr' WHERE id=4;
+
+
+ -- 1. Sélectionner toutes les lignes de la table users
+ -- SELECT * FROM users;
+ -- 2. Sélectionner uniquement la ligne où le prénom est Ada dans la table users
+-- SELECT * FROM users WHERE firstname = 'Ada';
+-- 3. Sélectionner les lignes dont la première lettre du prénom est un B dans la table users
+-- SELECT * FROM users WHERE firstname LIKE 'B%';
+-- 4. Compter le nombre de ligne qu’il y a dans la table users
+-- SELECT COUNT(*) AS [nombre des lignes] FROM users;
+    -- 5. Compter le nombre de ligne dont la première lettre du prénom est un B dans la table users
+    -- SELECT COUNT(firstname) FROM users WHERE firstname LIKE 'B%';
+-- 6. Afficher uniquement la colonne contenant le prénom de la table users.
+    --  SELECT firstname FROM users;
+
+-- 1. Insérer une ligne dans la table edusign qui correspond à la user Ada avec comme date de signature le 2024-05-30 09:30:00
+     -- INSERT INTO edusign (user_id, created_at) VALUES (1, DATETIME('2024-05-30 09:30:00'));
+
+-- 2. Insérer une ligne dans la table edusign qui correspond à la user Bella avec comme date de signature le 2024-05-30 09:30:00
+     -- INSERT INTO edusign (user_id, created_at) VALUES (2, DATETIME('2024-05-30 09:30:00'));
+-- 3. Insérer toutes les lignes de la table users dans la table edusign avec pour date de signature le 2024-09-01 09:30:00
+    -- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-09-01 09:30:00'));
+    -- INSERT INTO edusign (user_id, created_at) VALUES (4, DATETIME('2024-09-01 09:30:00'));
+-- 4. Sélectionner toutes les lignes de la table edusign ordonnées par date de signature de la plus récente à la plus ancienne et par user_id ascendant
+     --SELECT * FROM edusign ORDER BY created_at DESC;
+ -- 5. Sélectionner toutes les lignes de la table edusign dont la date est 2024-05-30 09:30:00
+        -- SELECT * FROM edusign WHERE created_at = '2024-05-30 09:30:00';
+ -- Compter le nombre de ligne au sein de la table edusign dont le prénom est Bella, le nom de la colonne finale doit être volume
+      --SELECT COUNT(*) AS [volume] FROM edusign WHERE user_id = 1;
+--SELECT * FROM edusign;
+-- SELECT created_at, COUNT(*) AS [e nombre d’apprenantes] FROM edusign GROUP BY created_at;
+-- UPDATE edusign SET user_id=1 WHERE id=1;
+-- UPDATE edusign SET user_id=3 WHERE id=2;
+-- UPDATE edusign SET user_id=1 WHERE id=3;
+-- UPDATE edusign SET user_id=4 WHERE id=4;
+-- INSERT INTO edusign (user_id, created_at) VALUES (2, DATETIME('2024-09-01 09:30:00'))
+-- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-09-01 09:30:00'))
+
+-- Création d'une nouvelle table edusign avec un FOREIGN KEY (user_id)
 --  CREATE TABLE edusign (
 --     id INTEGER PRIMARY KEY AUTOINCREMENT,
 --     user_id INTEGER NOT NULL,
@@ -30,9 +88,15 @@
 -- );
 
 -- INSERT INTO edusign (user_id, created_at) VALUES (1, DATETIME('2024-05-30 09:30:00'));
- -- INSERT INTO edusign (user_id, created_at) VALUES (2, DATETIME('2024-05-30 09:30:00'));
---  INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-09-01 09:30:00'));
+-- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-05-30 09:30:00'));
+-- INSERT INTO edusign (user_id, created_at) VALUES (1, DATETIME('2024-09-01 09:30:00'));
 -- INSERT INTO edusign (user_id, created_at) VALUES (4, DATETIME('2024-09-01 09:30:00'));
+-- INSERT INTO edusign (user_id, created_at) VALUES (2, DATETIME('2024-09-01 09:30:00'));
+-- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-05-30 09:30:00'));
 
- SELECT * FROM edusign;
+-- SELECT * FROM edusign;
+-- SELECT * FROM edusign ORDER BY created_at DESC, user_id ASC;
+-- SELECT * FROM edusign WHERE created_at = '2024-05-30 09:30:00' OR '2024-09-01 09:30:00';
 
+
+-- SELECT COUNT (*) FROM edusign JOIN users ON edusign.user_id = users.id WHERE firstname='Bella';
