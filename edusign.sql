bdb28b816c266465626bd089ab647575d35e71b7
Molid <delmolid@hotmail.fr>
2024-12-13 17:00:08 +0100

    code review

--- edusign.sql
+++ edusign.sql
@@ -36,27 +36,28 @@
 
 
  -- 1. Sélectionner toutes les lignes de la table users
- SELECT * FROM users;
+ -- SELECT * FROM users;
 
  -- 2. Sélectionner uniquement la ligne où le prénom est Ada dans la table users
 -- SELECT * FROM users WHERE firstname = 'Ada';
 
 -- 3. Sélectionner les lignes dont la première lettre du prénom est un B dans la table users
--- SELECT * FROM users WHERE firstname LIKE 'B%';
+ -- SELECT * FROM users WHERE firstname LIKE 'B%';
 
 -- 4. Compter le nombre de ligne qu’il y a dans la table users
 -- SELECT COUNT(*) AS [nombre des lignes] FROM users;
 
 -- 5. Compter le nombre de ligne dont la première lettre du prénom est un B dans la table users
-    -- SELECT COUNT(firstname) FROM users WHERE firstname LIKE 'B%';
+     -- SELECT COUNT(firstname) FROM users WHERE firstname LIKE 'B%';
 -- 6. Afficher uniquement la colonne contenant le prénom de la table users.
-    --  SELECT firstname FROM users;
+     -- SELECT firstname FROM users;
  -- Niveau moyen
 -- 1. Insérer une ligne dans la table edusign qui correspond à la user Ada avec comme date de signature le 2024-05-30 09:30:00
      -- INSERT INTO edusign (user_id, created_at) VALUES (1, DATETIME('2024-05-30 09:30:00'));
 
 -- 2. Insérer une ligne dans la table edusign qui correspond à la user Bella avec comme date de signature le 2024-05-30 09:30:00
          -- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-05-30 09:30:00'));
+
 -- 3. Insérer toutes les lignes de la table users dans la table edusign avec pour date de signature le 2024-09-01 09:30:00
     -- INSERT INTO edusign (user_id, created_at) VALUES (1, DATETIME('2024-09-01 09:30:00'));
     -- INSERT INTO edusign (user_id, created_at) VALUES (4, DATETIME('2024-09-01 09:30:00'));
@@ -64,10 +65,10 @@
     -- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-05-30 09:30:00'));
 
 -- 4. Sélectionner toutes les lignes de la table edusign ordonnées par date de signature de la plus récente à la plus ancienne et par user_id ascendant
-     -- SELECT * FROM edusign ORDER BY created_at DESC, user_id ASC;
+      -- SELECT * FROM edusign ORDER BY created_at DESC, user_id ASC;
 
  -- 5. Sélectionner toutes les lignes de la table edusign dont la date est 2024-05-30 09:30:00
-        -- SELECT * FROM edusign WHERE created_at = '2024-05-30 09:30:00';
+       --  SELECT * FROM edusign WHERE created_at = '2024-05-30 09:30:00';
 
 -- Niveau moyen+ (bonus)
 
@@ -75,5 +76,5 @@
     --  SELECT created_at, COUNT(*) AS [Nombre d’apprenantes] FROM edusign GROUP BY created_at;
 
  -- 2. Compter le nombre de ligne au sein de la table edusign dont le prénom est Bella, le nom de la colonne finale doit être volume
-    -- SELECT COUNT (*) FROM edusign JOIN users ON edusign.user_id = users.id WHERE firstname='Bella';
+     -- SELECT COUNT (*) AS [Volume] FROM edusign JOIN users ON edusign.user_id = users.id WHERE firstname='Bella';
 
