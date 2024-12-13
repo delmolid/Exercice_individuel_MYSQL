b27d9b60c206b7f7de80aad2011e0aedc6ebd617
Molid <delmolid@hotmail.fr>
2024-12-13 16:35:49 +0100

    ajout commentaires

---
+++ README.md
@@ -0,0 +1,17 @@
+-- Etape 2 : Structuration de la base de données
+
+-- Création de la table USERS :
+-- CREATE TABLE users (
+-- id INTEGER PRIMARY KEY AUTOINCREMENT,
+-- firstname TEXT NOT NULL,
+-- lastname TEXT NOT NULL,
+-- email TEXT NOT NULL UNIQUE
+-- );
+
+id	firstname	lastname	email
+1	Ada	Lovelace	ada@test.fr
+2	Beatrice	Worsley	bea@test.fr
+3	Bella	Guerin	bella@test.fr
+4	Barbara	Chase	barbara@test.fr
+
+
--- edusign.sql
+++ edusign.sql
@@ -1,3 +1,5 @@
+-- Etape 2 : Structuration de la base de données
+
 -- Création de la table USERS : 
 -- CREATE TABLE users (
 -- id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -6,22 +8,12 @@
 -- email TEXT NOT NULL UNIQUE 
 -- );
 
--- Création de la table edusign
--- CREATE TABLE edusign (
--- id INTEGER PRIMARY KEY AUTOINCREMENT,
--- firstname TEXT NOT NULL,
--- email TEXT NOT NULL UNIQUE,
--- created_at DATETIME NOT NULL
--- );
-
 -- Création des apprenantes dans la table users
 -- INSERT INTO users (firstname, lastname, email) VALUES ('Ada', 'Lovelace', 'ada@test.fr');
 -- INSERT INTO users (firstname, lastname, email) VALUES ('Paul', 'Martin', 'paul@test.fr');
 -- INSERT INTO users (firstname, lastname, email) VALUES ('Celine', 'Pierre', 'celine@test.fr');
 -- INSERT INTO users (firstname, lastname, email) VALUES ('Awaleh', 'Samatar', 'awaleh@test.fr');
 
--- Suppression de la prémiere table edusign 
---DROP TABLE edusign;
 
 -- Création d'une nouvelle table edusign avec un FOREIGN KEY (user_id)
 --  CREATE TABLE edusign (
@@ -45,58 +37,58 @@
 
  -- 1. Sélectionner toutes les lignes de la table users
  -- SELECT * FROM users;
+
  -- 2. Sélectionner uniquement la ligne où le prénom est Ada dans la table users
 -- SELECT * FROM users WHERE firstname = 'Ada';
+
 -- 3. Sélectionner les lignes dont la première lettre du prénom est un B dans la table users
 -- SELECT * FROM users WHERE firstname LIKE 'B%';
+
 -- 4. Compter le nombre de ligne qu’il y a dans la table users
 -- SELECT COUNT(*) AS [nombre des lignes] FROM users;
-    -- 5. Compter le nombre de ligne dont la première lettre du prénom est un B dans la table users
+
+-- 5. Compter le nombre de ligne dont la première lettre du prénom est un B dans la table users
     -- SELECT COUNT(firstname) FROM users WHERE firstname LIKE 'B%';
 -- 6. Afficher uniquement la colonne contenant le prénom de la table users.
     --  SELECT firstname FROM users;
-
+ -- Niveau moyen
 -- 1. Insérer une ligne dans la table edusign qui correspond à la user Ada avec comme date de signature le 2024-05-30 09:30:00
      -- INSERT INTO edusign (user_id, created_at) VALUES (1, DATETIME('2024-05-30 09:30:00'));
 
 -- 2. Insérer une ligne dans la table edusign qui correspond à la user Bella avec comme date de signature le 2024-05-30 09:30:00
-     -- INSERT INTO edusign (user_id, created_at) VALUES (2, DATETIME('2024-05-30 09:30:00'));
+         -- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-05-30 09:30:00'));
 -- 3. Insérer toutes les lignes de la table users dans la table edusign avec pour date de signature le 2024-09-01 09:30:00
-    -- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-09-01 09:30:00'));
+    -- INSERT INTO edusign (user_id, created_at) VALUES (1, DATETIME('2024-09-01 09:30:00'));
     -- INSERT INTO edusign (user_id, created_at) VALUES (4, DATETIME('2024-09-01 09:30:00'));
+    -- INSERT INTO edusign (user_id, created_at) VALUES (2, DATETIME('2024-09-01 09:30:00'));
+    -- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-05-30 09:30:00'));
+
 -- 4. Sélectionner toutes les lignes de la table edusign ordonnées par date de signature de la plus récente à la plus ancienne et par user_id ascendant
-     --SELECT * FROM edusign ORDER BY created_at DESC;
+     -- SELECT * FROM edusign ORDER BY created_at DESC, user_id ASC;
+
  -- 5. Sélectionner toutes les lignes de la table edusign dont la date est 2024-05-30 09:30:00
         -- SELECT * FROM edusign WHERE created_at = '2024-05-30 09:30:00';
- -- Compter le nombre de ligne au sein de la table edusign dont le prénom est Bella, le nom de la colonne finale doit être volume
-      --SELECT COUNT(*) AS [volume] FROM edusign WHERE user_id = 1;
---SELECT * FROM edusign;
--- SELECT created_at, COUNT(*) AS [e nombre d’apprenantes] FROM edusign GROUP BY created_at;
--- UPDATE edusign SET user_id=1 WHERE id=1;
--- UPDATE edusign SET user_id=3 WHERE id=2;
--- UPDATE edusign SET user_id=1 WHERE id=3;
--- UPDATE edusign SET user_id=4 WHERE id=4;
--- INSERT INTO edusign (user_id, created_at) VALUES (2, DATETIME('2024-09-01 09:30:00'))
--- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-09-01 09:30:00'))
 
--- Création d'une nouvelle table edusign avec un FOREIGN KEY (user_id)
---  CREATE TABLE edusign (
---     id INTEGER PRIMARY KEY AUTOINCREMENT,
---     user_id INTEGER NOT NULL,
---     created_at DATETIME NOT NULL,
---     FOREIGN KEY (user_id) REFERENCES users(id)
--- );
+-- Niveau moyen+ (bonus)
 
--- INSERT INTO edusign (user_id, created_at) VALUES (1, DATETIME('2024-05-30 09:30:00'));
--- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-05-30 09:30:00'));
--- INSERT INTO edusign (user_id, created_at) VALUES (1, DATETIME('2024-09-01 09:30:00'));
--- INSERT INTO edusign (user_id, created_at) VALUES (4, DATETIME('2024-09-01 09:30:00'));
--- INSERT INTO edusign (user_id, created_at) VALUES (2, DATETIME('2024-09-01 09:30:00'));
--- INSERT INTO edusign (user_id, created_at) VALUES (3, DATETIME('2024-05-30 09:30:00'));
+-- 1. Dans la table edusign, afficher le nombre d’apprenantes par date
+    --  SELECT created_at, COUNT(*) AS [Nombre d’apprenantes] FROM edusign GROUP BY created_at;
+
+ -- 2. Compter le nombre de ligne au sein de la table edusign dont le prénom est Bella, le nom de la colonne finale doit être volume
+    -- SELECT COUNT (*) FROM edusign JOIN users ON edusign.user_id = users.id WHERE firstname='Bella';
+
+
+SELECT * FROM users;
+
+
+
+
+
+
 
 -- SELECT * FROM edusign;
--- SELECT * FROM edusign ORDER BY created_at DESC, user_id ASC;
+
 -- SELECT * FROM edusign WHERE created_at = '2024-05-30 09:30:00' OR '2024-09-01 09:30:00';
 
 
--- SELECT COUNT (*) FROM edusign JOIN users ON edusign.user_id = users.id WHERE firstname='Bella';
+-- SELECT * FROM users;
